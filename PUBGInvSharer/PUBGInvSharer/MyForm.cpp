5c986d6c66c8b3ba3d2abfd5d3314d84b4a719f9
Kirin Patel <kirinpatel@gmail.com>
2017-08-02 21:34:59 -0400

    Starting work on opencv

--- PUBGInvSharer/PUBGInvSharer/MyForm.cpp
+++ PUBGInvSharer/PUBGInvSharer/MyForm.cpp
@@ -1,7 +1,24 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <Windows.h>
+#include <iostream>
 #include "MyForm.h"
 
+using namespace std;
 using namespace System;
 using namespace System::Windows::Forms; 
+
+void GetDesktopResolution(int& monitorWidth, int& monitorHeight)
+{
+	RECT desktop;
+	const HWND hDesktop = GetDesktopWindow();
+	GetWindowRect(hDesktop, &desktop);
+	monitorWidth = desktop.right;
+	monitorHeight = desktop.bottom;
+}
 
 int main(int, char**)
 {
@@ -10,5 +27,11 @@ int main(int, char**)
 
 	PUBGInvSharer::MyForm form;
 	Application::Run(%form);
+
+	int monitorWidth, monitorHeight;
+	GetDesktopResolution(monitorWidth, monitorHeight);
+	cv::Mat image, outputImg;
+
+	cout << monitorWidth << " " << monitorHeight;
 	return 0;
 }
